392e051b89c014961c7336e0da9b61b4271b4eae
ArchItalia <117321045+ArchItalia@users.noreply.github.com>
2022-11-05 11:46:37 +0000

    Add files via upload

--- conky/scripts/life.sh
+++ conky/scripts/life.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # author Jonathan Sanfilippo
-# inserisci la data di installazione del tuo sistema al posto di 20221007
+# head -1 /var/log/pacman.log | cut --complement -c 12-26 | cut --complement -c 1 | cut --complement -c 5,8 | cut --complement -c 9-14000000
+
 
 date=$(date +%Y%m%d)
-let DIFF=(`date +%s -d $date`-`date +%s -d  20221007`)/86400
+logdate=$( head -1 /var/log/pacman.log | cut --complement -c 12-26 | cut --complement -c 1 | cut --complement -c 5,8 | cut --complement -c 9-140000000000000000
+)
+let DIFF=(`date +%s -d $date`-`date +%s -d  $logdate`)/86400
 
 
 echo "$DIFF Days" 
